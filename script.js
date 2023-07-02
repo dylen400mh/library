f7d400ef1f33d774647bae5db324fd00a485e3bf
dylen400mh <dylenbelanger2004@gmail.com>
2023-07-02 01:46:29 -0400

    complete functionality for submitting book entries and displaying them on the page

--- script.js
+++ script.js
@@ -35,24 +35,24 @@ function displayBooks() {
         const card = document.createElement("div");
         card.classList.add("card");
 
-        const title = card.createElement("h3");
-        title.classList.add("card h3");
+        const title = document.createElement("h3");
+        title.classList.add("h3");
         title.textContent = book.title;
 
-        const author = card.createElement("h3");
-        author.classList.add("card h3");
+        const author = document.createElement("h3");
+        author.classList.add("h3");
         author.textContent = book.author;
 
-        const pages = card.createElement("h3");
-        pages.classList.add("card h3");
+        const pages = document.createElement("h3");
+        pages.classList.add("h3");
         pages.textContent = book.pages + " pages";
 
-        const readButton = card.createElement("button");
-        readButton.classList.add("card h3");
+        const readButton = document.createElement("button");
+        readButton.classList.add("h3");
         readButton.textContent = "Read";
 
-        const removeButton = card.createElement("button");
-        removeButton.classList.add("card h3");
+        const removeButton = document.createElement("button");
+        removeButton.classList.add("h3");
         removeButton.textContent = "Remove";
 
 
@@ -73,16 +73,39 @@ function displayBooks() {
 
 function openForm() {
     document.querySelector(".add-book-button").style.display = "none";
-    document.querySelector(".form-popup").style.display = "inline";
+    document.querySelector(".form-popup").style.display = "flex";
 }
 
-function submitForm() {
+// CLOSE POPUP FORM
+function closeForm() {
+    document.querySelector(".add-book-button").style.display = "block";
+    document.querySelector(".form-popup").style.display = "none";
+}
+
+// reset form values
+function resetFormValues() {
+    document.getElementById("title").value = "";
+    document.getElementById("author").value = "";
+    document.getElementById("pages").value = "";
+    document.getElementById("read").checked = false;
+}
+
+
+// submit form, retrieve values, and add book to library
+function submitForm(event) {
+
+    event.preventDefault();
+
     let title = document.getElementById("title").value;
     let author = document.getElementById("author").value;
     let pages = document.getElementById("pages").value;
-    let read = document.getElementById("read").value;
+    let read = document.getElementById("read").checked;
 
     addBookToLibrary(title, author, pages, read);
+    closeForm();
+    resetFormValues();
+
+    displayBooks();
 }
 
 
@@ -90,5 +113,5 @@ const container = document.querySelector(".container");
 const card = document.querySelector(".card");
 
 
-// displayBooks();
+displayBooks();
 
