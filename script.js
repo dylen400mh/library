bd4900ef342bc919fb9fee4c02e6b5de37c1341c
dylen400mh <dylenbelanger2004@gmail.com>
2023-07-07 21:30:23 -0400

    Change book constructor to a class

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
+
+
+
+
+
+
+
 let myLibrary = [];
 
 // CONSTRUCTOR
-function Book(title, author, pages, readStatus) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.readStatus = readStatus;
+class Book {
+    constructor(title, author, pages, readStatus) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.readStatus = readStatus;
+    }
 }
 
 // ADD BOOK
